b6980e410dfcfe0e84f7346551f8317263901b80
ahoeft1 <ahoeft1@C02016038747.wctc.msft>
2012-03-08 15:07:40 -0600

    Modified classes to allow spring to start functioning.  Moved xml file to correct location.  Still getting:  "Erroneous sym type: java.lang.Object.getTip"

--- src/tipservice/Startup_1.java
+++ src/tipservice/Startup_1.java
@@ -1,5 +1,8 @@
 package tipservice;
 
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * See if you can modify this class to use Spring as in Lab1.
  */
@@ -9,22 +12,26 @@ public class Startup_1 {
         // First create some low-level instances using
         // Liskov Substitution Principle
 
-        TipCalculator calc1 =
-           new FoodServiceTipCalculator(TipService.ServiceQuality.FAIR,125.00);
+//        TipCalculator calc1 =
+//           new FoodServiceTipCalculator(TipService.ServiceQuality.FAIR,125.00);
+//
+//        TipCalculator calc2 =
+//           new BaggageServiceTipCalculator(TipService.ServiceQuality.GOOD,5);
 
-        TipCalculator calc2 =
-           new BaggageServiceTipCalculator(TipService.ServiceQuality.GOOD,5);
+//        // Next, create the high-level class
+//        TipService tipMgr = new TipService(calc1);
 
-        // Next, create the high-level class
-        TipService tipMgr = new TipService(calc1);
+          final AbstractApplicationContext ctx = 
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
 
         // Now ask the tip manager to do some work...
-        double tip = tipMgr.getTip();
-        String quality = tipMgr.getServiceQuality();
+        double tip = ctx.getBean("tipManager").getTip();
+        String quality = ctx.getBean("tipManager").getServiceQuality();
 
         // Now prove it works...
         System.out.println("\nThe tip for the "
-                + calc1.getClass().getSimpleName() + " is: "
+                + ctx.getBean("tipCalc1").getClass().getSimpleName() + " is: "
                 + tip + ", with a service qualilty of: " + quality);
 
     }
